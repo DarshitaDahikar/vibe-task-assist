a03b255315c4e493aed0cb616c0f43b4c0a6d545
DarshitaDahikar <darshitadahikar@gmail.com>
2026-08-09 16:59:19 +0530

    Remove themeInitScript from NotFoundComponent
    Removed theme initialization script from the HTML head.

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -12,7 +12,6 @@ import { useEffect, type ReactNode } from "react";
 import appCss from "../styles.css?url";
 import { reportLovableError } from "../lib/lovable-error-reporting";
 import { Toaster } from "@/components/ui/sonner";
-import { themeInitScript } from "@/lib/theme";
 
 
 function NotFoundComponent() {
@@ -111,7 +110,6 @@ function RootShell({ children }: { children: ReactNode }) {
     <html lang="en">
       <head>
         <HeadContent />
-        <script dangerouslySetInnerHTML={{ __html: themeInitScript }} />
       </head>
       <body>
         {children}
